11a8298f57b5036283d4fafd0446394d77f5247a
LongAoTianxia <160115620+LongAoTianxia@users.noreply.github.com>
2025-06-20 00:16:00 +0800

    Add files via upload

---
+++ Vision Transformer/train.py
@@ -0,0 +1,169 @@
+import os
+import math
+import argparse
+import tarfile  # 新增导入
+import tempfile  # 新增导入
+
+import torch
+import torch.optim as optim
+import torch.optim.lr_scheduler as lr_scheduler
+from torch.utils.tensorboard import SummaryWriter
+from torchvision import transforms
+
+from my_dataset import MyDataSet
+#from vit_model import vit_base_patch16_224_in21k_Qua as create_model
+from vit_model import vit_base_patch16_224_in21k as create_model
+from utils import read_split_data, train_one_epoch, evaluate
+
+def extract_tar_gz(tar_path, extract_dir):
+    """解压 .tar.gz 文件到指定目录"""
+    with tarfile.open(tar_path, 'r:gz') as tar:
+        tar.extractall(path=extract_dir)
+    print(f"Extracted dataset to: {extract_dir}")
+
+def main(args):
+    device = torch.device(args.device if torch.cuda.is_available() else "cpu")
+
+    # 新增：处理 .tar.gz 数据集 ---------------------------------
+    original_data_path = args.data_path
+    temp_extract_dir = None
+
+    if args.data_path.endswith('.tar.gz'):
+        # 创建临时解压目录
+        temp_extract_dir = tempfile.mkdtemp()
+        print(f"Created temp directory: {temp_extract_dir}")
+
+        # 解压数据集
+        extract_tar_gz(args.data_path, temp_extract_dir)
+        args.data_path = temp_extract_dir  # 使用解压后的路径
+    # ---------------------------------------------------------
+
+    if os.path.exists("./weights") is False:
+        os.makedirs("./weights")
+
+    tb_writer = SummaryWriter()
+
+    train_images_path, train_images_label, val_images_path, val_images_label = read_split_data(args.data_path)
+
+    # 图片预处理 （224x224x3大小，并进行其他预处理）
+    data_transform = {
+        "train": transforms.Compose([transforms.RandomResizedCrop(224),
+                                     transforms.RandomHorizontalFlip(),
+                                     transforms.ToTensor(),
+                                     transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])]),
+        "val": transforms.Compose([transforms.Resize(256),
+                                   transforms.CenterCrop(224),
+                                   transforms.ToTensor(),
+                                   transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])])}
+
+    # 实例化训练数据集
+    train_dataset = MyDataSet(images_path=train_images_path,
+                              images_class=train_images_label,
+                              transform=data_transform["train"])
+
+    # 实例化验证数据集
+    val_dataset = MyDataSet(images_path=val_images_path,
+                            images_class=val_images_label,
+                            transform=data_transform["val"])
+
+    batch_size = args.batch_size
+    nw = min([os.cpu_count(), batch_size if batch_size > 1 else 0, 8])  # number of workers
+    print('Using {} dataloader workers every process'.format(nw))
+    train_loader = torch.utils.data.DataLoader(train_dataset,
+                                               batch_size=batch_size,
+                                               shuffle=True,
+                                               pin_memory=True,
+                                               num_workers=nw,
+                                               collate_fn=train_dataset.collate_fn)
+
+    val_loader = torch.utils.data.DataLoader(val_dataset,
+                                             batch_size=batch_size,
+                                             shuffle=False,
+                                             pin_memory=True,
+                                             num_workers=nw,
+                                             collate_fn=val_dataset.collate_fn)
+
+    # 创建模型
+    model = create_model(num_classes=args.num_classes, has_logits=False).to(device)  # 不需要Pre_Logits
+
+    # 加载预训练权重
+    if args.weights != "":
+        assert os.path.exists(args.weights), "weights file: '{}' not exist.".format(args.weights)
+        weights_dict = torch.load(args.weights, map_location=device)
+        # 删除不需要的权重
+        del_keys = ['head.weight', 'head.bias'] if model.has_logits \
+            else ['pre_logits.fc.weight', 'pre_logits.fc.bias', 'head.weight', 'head.bias']
+        for k in del_keys:
+            del weights_dict[k]
+        print(model.load_state_dict(weights_dict, strict=False))
+
+    # 冻结特征提取层
+    if args.freeze_layers:
+        for name, para in model.named_parameters():
+            # 除head, pre_logits外，其他权重全部冻结  只训练MLP Head中的权重
+            if "head" not in name and "pre_logits" not in name:
+                para.requires_grad_(False)
+            else:
+                print("training {}".format(name))
+
+    pg = [p for p in model.parameters() if p.requires_grad]
+    optimizer = optim.SGD(pg, lr=args.lr, momentum=0.9, weight_decay=5E-5)
+    # Scheduler https://arxiv.org/pdf/1812.01187.pdf
+    lf = lambda x: ((1 + math.cos(x * math.pi / args.epochs)) / 2) * (1 - args.lrf) + args.lrf  # cosine
+    scheduler = lr_scheduler.LambdaLR(optimizer, lr_lambda=lf)
+
+    for epoch in range(args.epochs):
+        # train
+        train_loss, train_acc = train_one_epoch(model=model,
+                                                optimizer=optimizer,
+                                                data_loader=train_loader,
+                                                device=device,
+                                                epoch=epoch)
+
+        scheduler.step()
+
+        # validate
+        val_loss, val_acc = evaluate(model=model,
+                                     data_loader=val_loader,
+                                     device=device,
+                                     epoch=epoch)
+
+        tags = ["train_loss", "train_acc", "val_loss", "val_acc", "learning_rate"]
+        tb_writer.add_scalar(tags[0], train_loss, epoch)
+        tb_writer.add_scalar(tags[1], train_acc, epoch)
+        tb_writer.add_scalar(tags[2], val_loss, epoch)
+        tb_writer.add_scalar(tags[3], val_acc, epoch)
+        tb_writer.add_scalar(tags[4], optimizer.param_groups[0]["lr"], epoch)
+
+        torch.save(model.state_dict(), "./weights/model-{}.pth".format(epoch))
+    # 新增：清理临时目录 ---------------------------------
+    #if temp_extract_dir and os.path.exists(temp_extract_dir):
+     #   print(f"Cleaning up temp directory: {temp_extract_dir}")
+     #   import shutil
+      #  shutil.rmtree(temp_extract_dir)
+    # -------------------------------------------------
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--num_classes', type=int, default=15)     # 修改，种类数num_classes
+    parser.add_argument('--epochs', type=int, default=10)
+    parser.add_argument('--batch-size', type=int, default=8)
+    parser.add_argument('--lr', type=float, default=0.001)
+    parser.add_argument('--lrf', type=float, default=0.01)
+
+    # 数据集所在根目录
+    # https://storage.googleapis.com/download.tensorflow.org/example_images/flower_photos.tgz
+    parser.add_argument('--data-path', type=str,
+                        default="D:/python/PycharmProjects/data/ball_15/train")
+    parser.add_argument('--model-name', default='', help='create model name')
+
+    # 预训练权重路径，如果不想载入就设置为空字符
+    parser.add_argument('--weights', type=str, default='D:/python/PycharmProjects/VIT_pretrained_weights/vit_base_patch16_224_in21k.pth',
+                        help='initial weights path')
+    # 是否冻结权重
+    parser.add_argument('--freeze-layers', type=bool, default=True)
+    parser.add_argument('--device', default='cuda:0', help='device id (i.e. 0 or 0,1 or cpu)')
+
+    opt = parser.parse_args()
+
+    main(opt)
