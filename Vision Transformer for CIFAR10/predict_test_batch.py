fdedb2d13c44a937c8b3b9ee544d0f86b75dafa8
LongAoTianxia <160115620+LongAoTianxia@users.noreply.github.com>
2025-06-24 16:23:05 +0800

    Update predict_test_batch.py

--- Vision Transformer for CIFAR10/predict_test_batch.py
+++ Vision Transformer for CIFAR10/predict_test_batch.py
@@ -75,7 +75,7 @@ def predict_and_create_submission():
 
     # 数据预处理
     data_transform = transforms.Compose([
-        transforms.Resize((224, 224)),
+        transforms.Resize((224, 224)),  # CIFAR-10是32x32，需要resize到224x224
         transforms.ToTensor(),
         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
     ])
